52f1e35fe244373cea8260d12899d232f2912607
SUFIYAN <155263639+muhammadsufiyanattari@users.noreply.github.com>
2025-01-21 09:50:58 +0500

    comment

---
+++ databaseUri.js
@@ -0,0 +1,48 @@
+import mongoose from 'mongoose';
+
+const mongooseDbUri = process.env.MONGO_URI 
+
+
+                 // connectDB boilerplate code
+const connectDB = async () => {
+    try {
+        // Connect to the MongoDB server
+        const connectionInstance = await mongoose.connect(mongooseDbUri, {
+            dbName: "my-todo",    }
+        );
+
+        console.log(`\n🌿 MongoDB connected ! 🍃\n`);
+ 
+        //agar connection fail ho to mongoose se error show karwaye ge
+        mongoose.connection.on(
+            "error",
+            console.error.bind(console, "Connection error:"),
+        );
+// server ko close karne se phele ye function chalye ge connection close karne ke lye
+        process.on("SIGINT", () => {
+            // Cleanup code
+            mongoose.connection.close();
+
+            console.log("Mongoose connection closed due to application termination");
+            process.exit(0);
+        });
+    } catch (error) {
+        // agar connection fail ho to server ko close karne ke lye
+        console.error("MONGODB connection FAILED ", error);
+        process.exit(1); // Exited with error
+    }
+};
+
+// (async () => {
+    try {
+        await connectDB();
+    
+        //       app.listen(PORT, () =>
+        //         console.log(`⚙️  Server running at port ==>> ${PORT}`),
+        //       );
+    
+        //       app.on("error", (err) => console.log("🚀 ~ main file:", err));
+    } catch (err) {
+        console.log("🚀 ~ main file ~ err:", err);
+    }
+    //   })(); self calling function
--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import express, { json } from "express";
+import 'dotenv/config'
 import cors from "cors";
+// test
+
+import "./databaseUri.js";
+import { Todo } from "./models/Schema.js";
 const app = express();
 const port = process.env.PORT || 3000; //jab backend deploy karte hen
 //  to ye zarori nahi ke wo jo routes chala rahe he same is lye ye karte hen
@@ -8,71 +13,96 @@ app.use(
     origin: ["http://localhost:5173", "https://sufiyan-todo-frontend.surge.sh"],
   })
 );
-let todos = [];
+
 app.use(json()); // ye routes ko cheak karta he agar hoga to response de deta he
 app.use(express.json()); //body incripted hoti he
 // usse format me change karne ke lye  app.use ye karte he
 
 // todo get requset jo user ko show karwni hi
-app.get("/getTodos", (req, res) => {
+app.get("/getTodos", async(req, res) => {
+ try {
+  const todos = await Todo.find({},
+    //{todoContent:1}1 means show this field and 0 means not show this field
+   {__v:0}   // 0 means not show this field
+     //first parameter is for filter and second is for projection
+
+  ).sort({_id:-1});
   const message = !todos.length ? "Empty Todos" : "Data fetch Successfully !";
+
   res.send({ data: todos, message });
+  
+ } catch (error) {
+  res.status(500).send(` Internal Server Error ! `);
+ }
 });
 
 // todo add karne ke lye use kar data add karwane ke lye
-app.post("/addTodo", (req, res) => {
-  // console.log(req.body);
+app.post("/addTodo",async (req, res) => {
+  try {
+    // console.log(req.body);
   const obj = {
-    todoContent: req.body.todo,
-    id: String(new Date().getTime()),
-    Creates: new Date().getDate(),
+    todoContent: req.body.todoContent,
+    
   };
-  todos.push(obj);
-  res.status(200).send({ message: "Add Todo Successfully ! ", data: obj }); //todo add ho gae to ye message show ho
+ const response=await  Todo.create(obj);
+  console.log(response);
+  // todos.push(obj);
+  res.status(200).send({ message: "Add Todo Successfully ! ", data: response }); //todo add ho gae to ye message show ho
+  } catch (error) {
+    res.status(500).send(` Internal Server Error ! `);
+  }
 });
 
 // selected  todo Edit karne ke lye
-app.patch("/editTodo/:id", (req, res) => {
+app.patch("/editTodo/:id", async(req, res) => {
+ try {
   const id = req.params.id;
-  let isFound = false;
-  for (let i = 0; i < todos.length; i++) {
-    todos[i];
-    if (todos[i].id === id) {
-      todos[i].todoContent = req.body.todoContent;
-      isFound = true;
-      break;
-    }
-  }
-  if (isFound) {
+  const result=await Todo.findByIdAndUpdate(id, { todoContent: req.body.todoContent });
+  // let isFound = false;
+  // for (let i = 0; i < todos.length; i++) {
+  //   todos[i];
+  //   if (todos[i].id === id) {
+  //     todos[i].todoContent = req.body.todoContent;
+  //     isFound = true;
+  //     break;
+  //   }
+  // }
+  console.log(`result`, result);
+  
+  if (result) {
     res.status(201).send({
-      data: { todoContent: req.body.todoContent, id },
+      data: result,
       message: "Todo Update Successfully !",
     });
   } else {
     res.status(201).send({ data: null, message: "Todo id Not Found !" });
   }
+ } catch (error) {
+  res.status(500).send(` Internal Server Error ! `);
+ }
 });
 
 // selected  todo Delet karne ke lye
 
-app.delete("/deletTodo/:id", (req, res) => {
+app.delete("/deletTodo/:id",async (req, res) => {
   // res.send("hello");
 
   const id = req.params.id;
-  let isFound = false;
-  for (let i = 0; i < todos.length; i++) {
+  const result=await Todo.findByIdAndDelete(id);
+  // let isFound = false;
+  // for (let i = 0; i < todos.length; i++) {
 
-    todos[i];
-    if (todos[i].id === id) {
-      todos.splice(i, 1);
+  //   todos[i];
+  //   if (todos[i].id === id) {
+  //     todos.splice(i, 1);
       
-      isFound = true;
-      break;
+  //     isFound = true;
+  //     break;
 
-    }
-  }
+  //   }
+  // }
   
-  if (isFound) {
+  if (result) {
     res.status(200).send({
       data: { id },
       message: "Todo Deleted Successfully !",
